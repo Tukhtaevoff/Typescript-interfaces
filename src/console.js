c86de6f72eb16c0ea7f00015e562ff743dcf62ba
Tukhtaev27 <108573349+Tukhtaev27@users.noreply.github.com>
2022-11-16 21:50:12 +0500

    js added

---
+++ src/console.js
@@ -0,0 +1,4 @@
+console.log("This is how typescript works like JavaScript")
+
+let programmingLangs = ["Java", "Python", "C#", "C++", "C", "Go", "Typescript", "R"];
+console.log(programmingLangs[6] + " " + "is my favourite programming language ever !");
