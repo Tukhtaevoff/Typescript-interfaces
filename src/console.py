89cce3093f9152a9d6a88e0c55f48952178cf6dd
Tukhtaev27 <108573349+Tukhtaev27@users.noreply.github.com>
2022-11-16 21:59:16 +0500

    python added

---
+++ src/console.py
@@ -0,0 +1,13 @@
+name = "Abdulaziz"
+
+if name == "Abdulaziz": 
+    print("You are owner of this project")
+elif name == "User":
+    print("You are not owner of this project")
+else :
+    print("This is not yours !")
+
+progLangs = ["Ruby", "Python", "Java", "PHP", "C", "C#", "C++", "Java", "JavaScript", "TypeScript", "Go", "Nodejs"];
+
+for i in range(len(progLangs)): 
+    print(progLangs[i])
